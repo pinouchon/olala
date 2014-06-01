530692c9e2667330c9485f1a085f754a68ad8f65
pinouchon <pinouchon@gmail.com>
2014-06-02 01:09:10 +0200

    helper ok

--- lib/olala.rb
+++ lib/olala.rb
@@ -7,7 +7,11 @@ module Olala
   end
 
   class Engine < Rails::Engine
-    config.autoload_paths << File.expand_path("../app", __FILE__)
+    #config.autoload_paths << File.expand_path("../app", __FILE__)
+
+    config.to_prepare do
+      ApplicationController.helper(Olala::Helpers)
+    end
   end
 
   module CustomAuth
@@ -24,5 +28,17 @@ module Olala
       end
     end
 
+  end
+
+  module Helpers
+    def editable(label, default = 'undefined', &block)
+      if block_given?
+        raw "<div class='editable' data-label='#{label.to_s}'>" +
+                Olala::Label.retrieve(label, capture(&block)) +
+                '</div>'.html_safe
+      else
+        raw "<div class='editable' data-label='#{label.to_s}'>#{Olala::Label.retrieve(label, default)}</div>".html_safe
+      end
+    end
   end
 end
